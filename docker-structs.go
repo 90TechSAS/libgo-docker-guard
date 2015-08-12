097fb35902ab80f2ed82d35b2a3adf9f93a54570
Nurza <gabriel.mgt0+git@gmail.com>
2015-08-12 15:49:31 +0200

    Add Container struct

--- docker-structs.go
+++ docker-structs.go
@@ -48,3 +48,122 @@ type DockerVersion struct {
 	Os            string `json:"Os"`
 	Version       string `json:"Version"`
 }
+
+type Container struct {
+	AppArmorProfile string   `json:"AppArmorProfile"`
+	Args            []string `json:"Args"`
+	Config          struct {
+		AttachStderr    bool        `json:"AttachStderr"`
+		AttachStdin     bool        `json:"AttachStdin"`
+		AttachStdout    bool        `json:"AttachStdout"`
+		Cmd             []string    `json:"Cmd"`
+		Domainname      string      `json:"Domainname"`
+		Entrypoint      []string    `json:"Entrypoint"`
+		Env             []string    `json:"Env"`
+		ExposedPorts    struct{}    `json:"ExposedPorts"`
+		Hostname        string      `json:"Hostname"`
+		Image           string      `json:"Image"`
+		Labels          struct{}    `json:"Labels"`
+		MacAddress      string      `json:"MacAddress"`
+		NetworkDisabled bool        `json:"NetworkDisabled"`
+		OnBuild         interface{} `json:"OnBuild"`
+		OpenStdin       bool        `json:"OpenStdin"`
+		PortSpecs       interface{} `json:"PortSpecs"`
+		StdinOnce       bool        `json:"StdinOnce"`
+		Tty             bool        `json:"Tty"`
+		User            string      `json:"User"`
+		VolumeDriver    string      `json:"VolumeDriver"`
+		Volumes         interface{} `json:"Volumes"`
+		WorkingDir      string      `json:"WorkingDir"`
+	} `json:"Config"`
+	Created    string      `json:"Created"`
+	Driver     string      `json:"Driver"`
+	ExecDriver string      `json:"ExecDriver"`
+	ExecIDs    interface{} `json:"ExecIDs"`
+	HostConfig struct {
+		Binds           []string      `json:"Binds"`
+		BlkioWeight     int           `json:"BlkioWeight"`
+		CapAdd          interface{}   `json:"CapAdd"`
+		CapDrop         interface{}   `json:"CapDrop"`
+		CgroupParent    string        `json:"CgroupParent"`
+		ContainerIDFile string        `json:"ContainerIDFile"`
+		CPUPeriod       int           `json:"CpuPeriod"`
+		CPUQuota        int           `json:"CpuQuota"`
+		CPUShares       int           `json:"CpuShares"`
+		CpusetCpus      string        `json:"CpusetCpus"`
+		CpusetMems      string        `json:"CpusetMems"`
+		Devices         []interface{} `json:"Devices"`
+		DNS             interface{}   `json:"Dns"`
+		DNSSearch       interface{}   `json:"DnsSearch"`
+		ExtraHosts      interface{}   `json:"ExtraHosts"`
+		IpcMode         string        `json:"IpcMode"`
+		Links           interface{}   `json:"Links"`
+		LogConfig       struct {
+			Config struct{} `json:"Config"`
+			Type   string   `json:"Type"`
+		} `json:"LogConfig"`
+		LxcConf         []interface{} `json:"LxcConf"`
+		Memory          int           `json:"Memory"`
+		MemorySwap      int           `json:"MemorySwap"`
+		NetworkMode     string        `json:"NetworkMode"`
+		OomKillDisable  bool          `json:"OomKillDisable"`
+		PidMode         string        `json:"PidMode"`
+		PortBindings    struct{}      `json:"PortBindings"`
+		Privileged      bool          `json:"Privileged"`
+		PublishAllPorts bool          `json:"PublishAllPorts"`
+		ReadonlyRootfs  bool          `json:"ReadonlyRootfs"`
+		RestartPolicy   struct {
+			MaximumRetryCount int    `json:"MaximumRetryCount"`
+			Name              string `json:"Name"`
+		} `json:"RestartPolicy"`
+		SecurityOpt interface{} `json:"SecurityOpt"`
+		UTSMode     string      `json:"UTSMode"`
+		Ulimits     interface{} `json:"Ulimits"`
+		VolumesFrom interface{} `json:"VolumesFrom"`
+	} `json:"HostConfig"`
+	HostnamePath    string `json:"HostnamePath"`
+	HostsPath       string `json:"HostsPath"`
+	ID              string `json:"Id"`
+	Image           string `json:"Image"`
+	LogPath         string `json:"LogPath"`
+	MountLabel      string `json:"MountLabel"`
+	Name            string `json:"Name"`
+	NetworkSettings struct {
+		Bridge                 string      `json:"Bridge"`
+		EndpointID             string      `json:"EndpointID"`
+		Gateway                string      `json:"Gateway"`
+		GlobalIPv6Address      string      `json:"GlobalIPv6Address"`
+		GlobalIPv6PrefixLen    int         `json:"GlobalIPv6PrefixLen"`
+		HairpinMode            bool        `json:"HairpinMode"`
+		IPAddress              string      `json:"IPAddress"`
+		IPPrefixLen            int         `json:"IPPrefixLen"`
+		IPv6Gateway            string      `json:"IPv6Gateway"`
+		LinkLocalIPv6Address   string      `json:"LinkLocalIPv6Address"`
+		LinkLocalIPv6PrefixLen int         `json:"LinkLocalIPv6PrefixLen"`
+		MacAddress             string      `json:"MacAddress"`
+		NetworkID              string      `json:"NetworkID"`
+		PortMapping            interface{} `json:"PortMapping"`
+		Ports                  struct{}    `json:"Ports"`
+		SandboxKey             string      `json:"SandboxKey"`
+		SecondaryIPAddresses   interface{} `json:"SecondaryIPAddresses"`
+		SecondaryIPv6Addresses interface{} `json:"SecondaryIPv6Addresses"`
+	} `json:"NetworkSettings"`
+	Path           string `json:"Path"`
+	ProcessLabel   string `json:"ProcessLabel"`
+	ResolvConfPath string `json:"ResolvConfPath"`
+	RestartCount   int    `json:"RestartCount"`
+	State          struct {
+		Dead       bool   `json:"Dead"`
+		Error      string `json:"Error"`
+		ExitCode   int    `json:"ExitCode"`
+		FinishedAt string `json:"FinishedAt"`
+		OOMKilled  bool   `json:"OOMKilled"`
+		Paused     bool   `json:"Paused"`
+		Pid        int    `json:"Pid"`
+		Restarting bool   `json:"Restarting"`
+		Running    bool   `json:"Running"`
+		StartedAt  string `json:"StartedAt"`
+	} `json:"State"`
+	Volumes   struct{} `json:"Volumes"`
+	VolumesRW struct{} `json:"VolumesRW"`
+}
