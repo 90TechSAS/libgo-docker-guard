43314fa503568e60d548bcb85af652d9b68cd203
Nurza <gabriel.mgt0+git@gmail.com>
2015-08-21 13:45:29 +0200

    Add Event struct + Event types

---
+++ events.go
@@ -0,0 +1,18 @@
+package lib
+
+const (
+	DiskSpaceLimitReached   = iota
+	MemorySpaceLimitReached = iota
+	ContainerStarted        = iota
+	ContainerStopped        = iota
+	ContainerRemoved        = iota
+	DiskIOOverload          = iota
+	NetBandwithOverload     = iota
+	CPUUsageOverload        = iota
+)
+
+type Event struct {
+	Type   int
+	Target string
+	Data   string
+}
